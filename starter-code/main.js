ef3692e02293c0d03521449dcfd4f92d0dc67d2c
Emel Lopez <emlopez88@emels-air.wifi.lagcc>
2017-03-24 14:00:01 -0400

    memmory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,20 +1,48 @@
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king"; 
 
-if (cardOne === cardThree){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-};
+var cards = ["queen", "queen", "king", "king"];
 
-var board = document.getElementById("game-board");
+var gameBoard = document.getElementById("game-board");
+
 function createCard(){
-	for (var x = 0; x < 4; x += 1){
-		var div = document.createElement("div");
-		div.className = "card";
-		board.appendChild(div);
+
+for (var i = 0; i < cards.length; i++){
+	var cardOne = document.createElement("div");
+	cardOne.className = "card";
+	gameBoard.appendChild(cardOne)
+	cardOne.setAttribute("data-card", cards[i]);
+	cardOne.addEventListener("click", isTwoCards)
+}
+}
+var cardsInPlay = [];
+
+function isTwoCards() {
+	if (this.getAttribute("data-card") === "queen") {
+		this.innerHTML = '<img src="queen.png" alt="Queen of Hearts" />';
+	}   if (this.getAttribute("data-card") === "king"){
+		this.innerHTML = '<img src="king.png" alt="King of Diamonds" />';
+	}
+
+	cardsInPlay.push(this.getAttribute("data-card"));
+	if (cardsInPlay.length === 2) {
+		isMatch();
+		cardsInPlay = [];
+} 
+}
+
+function isMatch(cards) {
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+
+	alert("You found a match!");
+
+} else {
+
+	alert("Sorry, try again.");
 	}
 }
 createCard();
